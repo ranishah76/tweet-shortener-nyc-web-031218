ce08bfd9040e082200aaf76f536f870fbd95800e
Rani Shah <rani76@gmail.com>
2018-02-28 16:21:15 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,7 +14,7 @@ def dictionary
   }
 end
 
-def word_substituter
+def word_substituter(tweet)
   tweet.split.collect do |word|
     if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
